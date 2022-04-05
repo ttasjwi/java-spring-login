c2e746268cc61acd38be8d4407681b8ce382272f
ttasjwi <ttasjwi920@gmail.com>
2022-04-05 23:16:22 +0900

    세션 구현하기
    - 세션 생성 : sessionId 및 쿠키 생성
    - 세션 조회 : 요청의 sessionId 쿠키값을 읽고 세션 저장소에 보관한 데이터 조회
    - 세션 만료 : 요청의 sessionId 쿠키값을 읽고, 세션 저장소에 보관한 sessionId 제거

---
+++ src/main/java/hello/login/web/session/SessionManager.java
@@ -0,0 +1,77 @@
+package hello.login.web.session;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 세션 관리
+ */
+@Component
+public class SessionManager {
+
+    public static final String SESSION_COOKIE_NAME = "mySessionId";
+    private final Map<String, Object> sessionStore;
+
+    public SessionManager() {
+        this.sessionStore = new ConcurrentHashMap<>();
+    }
+
+    /**
+     * 세션 생성
+     * - sessionId 생성 (임의의 추정불가능한 랜덤 값)
+     * - 세션 저장소에 sessionId와 보관할 값 저장
+     * - sessionId로 응답 쿠키를 생성해서 클라이언트에 전달
+     */
+
+    public void createSession(Object value, HttpServletResponse response) {
+
+        // sessionId 생성하고, 값을 세션에 저장
+        String sessionId = UUID.randomUUID().toString();
+        sessionStore.put(sessionId, value);
+
+        // 쿠키 세션
+        Cookie mySessionCookie = new Cookie(SESSION_COOKIE_NAME, sessionId);
+        response.addCookie(mySessionCookie);
+    }
+
+    public Object getSession(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if (sessionCookie == null) {
+            return null;
+        }
+        return sessionStore.get(sessionCookie.getValue());
+    }
+
+    /**
+     * 세션 만료
+     */
+    public void expire(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+
+        if (sessionCookie != null) {
+            sessionStore.remove(sessionCookie.getValue());
+        }
+    }
+
+
+    public Cookie findCookie(HttpServletRequest request, String cookieName) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            return null;
+        }
+        return Arrays.stream(cookies)
+                .filter(cookie -> cookie.getName().equals(SESSION_COOKIE_NAME))
+                .findAny()
+                .orElse(null);
+    }
+
+
+}
